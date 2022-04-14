1c77c3b2f35a62bce3a3573137fef6029d6c9e17
Wildiney Di Masi <wildiney@gmail.com>
2022-04-13 21:56:39 -0300

    update class

--- meu_ponto.py
+++ meu_ponto.py
@@ -1,4 +1,4 @@
-from classes.folha_de_ponto import Folha_de_Ponto
+from classes.adp import Folha_de_Ponto
 import config.config as config
 
 Folha_de_Ponto(config.login, config.senha, config.path, True)
